67e7cbfc0f6c7b7e3a2eb6b886b17c0554b67af1
mfathirr <mulfathir632@gmail.com>
2024-04-30 07:31:16 +0700

    managing route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:restaurant_app/data/model/restaurant.dart';
+import 'package:restaurant_app/home_page.dart';
+import 'package:restaurant_app/restaurant_detail_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,38 +19,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Restaurant'),
-        ),
-        body: FutureBuilder(
-          future: DefaultAssetBundle.of(context)
-              .loadString('assets/local_restaurant.json'),
-          builder: (context, snapshot) {
-            final List<Restaurant> restaurants = parseRestaurant(snapshot.data);
-            return ListView.builder(
-              itemCount: restaurants.length,
-              itemBuilder: (BuildContext context, int index) {
-                return _buildRestaurantList(restaurants[index]);
-              },
-            );
-          },
-        ),
-      ),
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => const HomePage(),
+        DetailPage.routeName: (context) => DetailPage(
+            restaurants:
+                ModalRoute.of(context)?.settings.arguments as Restaurant)
+      },
     );
-  }
-
-  ListTile _buildRestaurantList(Restaurant restaurants) {
-    return ListTile(
-        leading: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Image.network(
-            restaurants.pictureId,
-            fit: BoxFit.cover,
-            width: 100,
-          ),
-        ),
-        title: Text(restaurants.name),
-        subtitle: Text(restaurants.city));
   }
 }
